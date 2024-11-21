251ac36247d4d5780ead9d8ec271353a4c53ba1e
ncuy0110 <nguyenchonuy2001@gmail.com>
2024-11-22 04:16:10 +0700

    init

---
+++ Assets/Scripts/GamePlayPanel.cs
@@ -0,0 +1,52 @@
+﻿
+
+using System;
+using Game;
+using dotmob;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamePlayPanel : ShowHidable
+{
+    [SerializeField] private Text _lvlTxt;
+    [SerializeField] private Text txtTutorial;
+
+    private void Start()
+    {
+        _lvlTxt.text = $" LEVEL\n {LevelManager.Instance.Level.no}";
+
+        //Debug.Log("Level :" + LevelManager.Instance.Level.no);
+    }
+
+    public void OnClickUndo()
+    {
+        LevelManager.Instance.OnClickUndo();
+    }
+
+    public void OnClickRestart()
+    {
+        GameManager.LoadGame(new LoadGameData
+        {
+            Level = LevelManager.Instance.Level,
+            GameMode = LevelManager.Instance.GameMode,
+        },false);
+    }
+
+    public void OnClickMenu()
+    {
+        //SharedUIManager.PopUpPanel.ShowAsConfirmation("PAUSE", "Are you sure want to exit the game?", success =>
+        // {
+        //     if (!success)
+        //         return;
+
+        //     GameManager.LoadScene("MainMenu");
+        // });
+
+        SharedUIManager.PausePanel.Show();
+
+    }
+
+    private void Update()
+    {
+    }
+}
---
+++ Assets/Scripts/LevelCompletePanel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Game;
+using dotmob;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelCompletePanel : ShowHidable
+{
+    [SerializeField] private Text _toastTxt;
+    [SerializeField] private Text _levelTxt;
+    [SerializeField]private List<string> _toasts = new List<string>();
+
+
+
+    private void Awake()
+    {
+        _levelTxt.text = $" Level {LevelManager.Instance.Level.no}"; 
+    }
+
+    protected override void OnShowCompleted()
+    {
+        base.OnShowCompleted();
+
+        AdsManager.ShowOrPassAdsIfCan();
+    }
+
+
+    public void OnClickContinue()
+    {
+        UIManager.Instance.LoadNextLevel();
+    }
+
+    public void OnClickMainMenu()
+    {
+        GameManager.LoadScene("MainMenu");
+        SharedUIManager.PausePanel.Hide();
+    }
+}
